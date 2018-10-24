d9d6835bf8b5a341edbb8cda83ed8720ba13e6cf
Daniel Fegan <daniel.fegan@moneysupermarket.com>
2018-10-24 10:52:11 +0100

    initial commit

---
+++ src/main/java/com/moneysupermarket/profilesapitdd/ProfilesApiTddApplication.java
@@ -0,0 +1,12 @@
+package com.moneysupermarket.profilesapitdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfilesApiTddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfilesApiTddApplication.class, args);
+	}
+}
